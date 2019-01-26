d2c1d589220e95de42ae68a5b61b586782bd15e1
Ahmed3219 <46965808+Ahmed3219@users.noreply.github.com>
2019-01-27 01:34:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '@'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,4 +12,43 @@ client.on('message', msg => {
   }
 });
 
+const gamestats = [`@help`,`by bOmA CF`,``,``]
+var index = 0
+var timer = 10
+client.on("ready", ()=> {
+        setInterval(function(){
+        client.user.setGame(`${gamestats[index]}`,'https://www.twitch.tv/bomacf') 
+        index++
+            if( index >= gamestats.length) index = 0 ;
+        }, timer*1000);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
 client.login('NTM3NjI4MzgzMTE1MzQ1OTQy.Dy5rcg.geOIl5VrnMQmweiW6NN60FtPpSw');
